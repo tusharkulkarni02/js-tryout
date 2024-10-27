41fac03808220b6992929bdd66458ccf4e659913
tusharkulkarni02 <142671474+tusharkulkarni02@users.noreply.github.com>
2024-10-27 10:41:18 +0000

    Added datea and numbere files

---
+++ Js01/NumberaAndMaths.js
@@ -0,0 +1,40 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+/* To generat a number within a given range , the given function is Math.random() => generates a number in the range 1 to 0
+Math.floor() will give the closest left integer of the number
+(max - min + 1) is the range in which we want the number to be generated in.
++ min is done because we want alteast the generated numbers to greater than or equal to the min number in the given range
+*/
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min) 
---
+++ Js01/dates.js
@@ -0,0 +1,42 @@
+let myDate = new Date()
+
+console.log("This is a date Object:")
+console.log(myDate)
+console.log('\n')
+
+console.log("This is a toString method of date:")
+console.log(myDate.toString())
+
+console.log("This is a toDateString method of date:")
+console.log(myDate.toDateString())
+
+console.log("This is a toISOString method of date:")
+console.log(myDate.toISOString())
+
+console.log("This is a toJS0N method of date:")
+console.log(myDate.toJSON())
+
+console.log("This is a toLocaleDateString method of date:")
+console.log(myDate.toLocaleDateString())
+
+console.log("This is a toLocaleString method of date:")
+console.log(myDate.toLocaleString())
+
+/*
+This is a date Object:
+2024-10-27T06:41:47.772Z
+
+
+This is a toString method of date:
+Sun Oct 27 2024 06:41:47 GMT+0000 (Coordinated Universal Time)
+This is a toDateString method of date:
+Sun Oct 27 2024
+This is a toISOString method of date:
+2024-10-27T06:41:47.772Z
+This is a toJS0N method of date:
+2024-10-27T06:41:47.772Z
+This is a toLocaleDateString method of date:
+10/27/2024
+This is a toLocaleString method of date:
+10/27/2024, 6:41:47 AM
+*/
