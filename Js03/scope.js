3d74cecc52f7d68ecccd58c8e6f28bbc54cf53c3
tusharkulkarni02 <142671474+tusharkulkarni02@users.noreply.github.com>
2024-10-28 17:39:49 +0000

    Added scope

---
+++ Js03/scope.js
@@ -0,0 +1,73 @@
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+/* NOTE : The scope of a variable inside a browser is different as compared to the scope of a variable in node environment
+    i.e core scope 
+*/
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5)) // allowed
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5) // not allowed
+const addTwo = function(num){
+    return num + 2
+}
+
+/* NOTE: The above examples in javascript differ in the way of decalaration of a function
+        1st declaration -> declares the function without storing it in variable 
+                        -> we can call the first declaration before the code anywhere
+
+
+        2nd declaration -> declares the function and stores it in variable 
+                        -> calling the second declaration form is only allowed after the declaration
+*/
+                        
+
